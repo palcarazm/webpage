f87566d637f36aba96a78dd014b3bea8149a2487
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2023-09-17 19:33:14 +0200

    style: Improvements to horizontal lists

--- src/components/global/HorizontalList.jsx
+++ src/components/global/HorizontalList.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 import "./styles/horizontal-list.css";
 
-export default function HorizontalList({ className, children }) {
+function HorizontalList({ className, size = "medium", children }) {
   return (
-    <div className={`d-flex flex-wrap horizontal-list ${className}`}>
+    <div
+      className={`d-flex flex-wrap horizontal-list ${className}`}
+      style={{ fontSize: size }}
+    >
       {children}
     </div>
   );
 }
+
+HorizontalList.propTypes = {
+  className: PropTypes.string,
+  size: PropTypes.oneOf(["small", "medium"]),
+  children: PropTypes.node.isRequired,
+};
+
+export default HorizontalList;

dbfe783f3fb5ee7f2cf61f5fec4d898aa8d1e60d
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2023-09-17 11:02:24 +0200

    styles: Add color

--- src/components/cv/TimelineCard.jsx
+++ src/components/cv/TimelineCard.jsx
@@ -4,7 +4,7 @@ import "./styles/timeline-card.css";
 export function TimelineCard({ title, children }) {
   return (
     <div className="timeline-card">
-      <h3 className="m-0">{title}</h3>
+      <h3 className="m-0 text-primary">{title}</h3>
       {children}
     </div>
   );

e3e1dd7950aa8e16293bf1fd26a60eed8c2a3076
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2023-09-19 20:54:59 +0200

    feat: Add theme support to projects

--- src/components/projects/ProjectGrid.jsx
+++ src/components/projects/ProjectGrid.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
 import { Col, Row } from "react-bootstrap";
 import { Project } from "../../assets/projects";
 import { ProjectCell } from "./ProjectCell";
+import { ThemeContext, Themes } from "../../plugins";
 
 export function ProjectGrid({ projects, className = "" }) {
+  const { themeState } = useContext(ThemeContext);
+  const isLight = Themes.light === themeState.theme;
   return (
     <Row xs={1} lg={2} className={className}>
       {projects.map((project) => (
@@ -12,7 +15,7 @@ export function ProjectGrid({ projects, className = "" }) {
           key={`${project.username}/${project.repo}`}
           className="text-center mb-2"
         >
-          <ProjectCell project={project} />
+          <ProjectCell project={project} isLight={isLight} />
         </Col>
       ))}
     </Row>

871bd33c305215f87da011d0baf751afd0dd29e0
Pablo Alcaraz Martínez <pablo@alcarazm.es>
2023-10-14 10:02:06 +0200

    feat: Add skills

--- src/assets/skills/data/commons.jsx
+++ src/assets/skills/data/commons.jsx
@@ -1,13 +1,30 @@
 import Skill from "../model/Skill";
 import CategoryRepository from "./CategoryRepository";
 
-const commons = [
+const dev = [
   new Skill("HTML", 5, CategoryRepository.get("dev")),
   new Skill("CSS", 5, CategoryRepository.get("dev")),
-  new Skill("JS ECMAS", 4, CategoryRepository.get("dev")),
-  new Skill("JS jQuery", 4, CategoryRepository.get("dev")),
+  new Skill("JS", 4, CategoryRepository.get("dev")),
   new Skill("SQL", 4, CategoryRepository.get("dev")),
   new Skill("PHP", 2, CategoryRepository.get("dev")),
 ];
+
+const frameworks = [
+  new Skill("Bootstrap", 5, CategoryRepository.get("frameworks")),
+  new Skill("jQuery", 4, CategoryRepository.get("frameworks")),
+  new Skill("ReactJS", 3, CategoryRepository.get("frameworks")),
+  new Skill("Cypress", 4, CategoryRepository.get("frameworks")),
+];
+
+const software = [
+  new Skill("Jira", 5, CategoryRepository.get("software")),
+  new Skill("Confluence", 5, CategoryRepository.get("software")),
+  new Skill("Jira XRay", 3, CategoryRepository.get("software")),
+  new Skill("Microfocus ALM", 3, CategoryRepository.get("software")),
+  new Skill("Postman", 3, CategoryRepository.get("software")),
+  new Skill("PlantUML", 4, CategoryRepository.get("software")),
+];
+
+const commons = [...dev, ...frameworks, ...software];
 
 export default commons;
